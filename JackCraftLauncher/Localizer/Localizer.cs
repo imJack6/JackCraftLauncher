b7d5fd5e845fa45940fbf4d0398b7a7cf8a05906
imJack6 <3486688394@qq.com>
2023-06-29 22:55:42 +0800

    📝 完善 i18n

--- JackCraftLauncher/Localizer/Localizer.cs
+++ JackCraftLauncher/Localizer/Localizer.cs
@@ -3,9 +3,7 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.IO;
 using System.Text;
-using Avalonia;
 using Avalonia.Platform;
-using Newtonsoft.Json;
 
 namespace JackCraftLauncher.Localizer;
 
