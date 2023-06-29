7d47fa9617cdc7fba782681af0d97485de1571bc
imJack6 <3486688394@qq.com>
2023-06-29 22:57:59 +0800

    💾 仅存储

---
+++ JackCraftLauncher/Views/MainMenus/DownloadMenu.axaml.cs
@@ -0,0 +1,17 @@
+﻿using Avalonia.Controls;
+using Avalonia.Markup.Xaml;
+
+namespace JackCraftLauncher.Views.MainMenus;
+
+public partial class DownloadMenu : UserControl
+{
+    public DownloadMenu()
+    {
+        InitializeComponent();
+    }
+
+    private void InitializeComponent()
+    {
+        AvaloniaXamlLoader.Load(this);
+    }
+}
