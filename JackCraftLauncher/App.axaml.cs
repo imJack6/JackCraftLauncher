830f2e5ccf9ed9909cb1b74ade01089eac74f193
imJack6 <3486688394@qq.com>
2023-06-29 22:58:39 +0800

    🆙 添加核心初始化

--- JackCraftLauncher/App.axaml.cs
+++ JackCraftLauncher/App.axaml.cs
@@ -1,13 +1,16 @@
 using Avalonia;
 using Avalonia.Controls.ApplicationLifetimes;
 using Avalonia.Markup.Xaml;
-using JackCraftLauncher.ViewModels;
+using JackCraftLauncher.Class.Launch;
 using JackCraftLauncher.Views;
+using ProjBobcat.DefaultComponent.Launch.GameCore;
 
 namespace JackCraftLauncher;
 
 public class App : Application
 {
+    public static DefaultGameCore Core { get; private set; } = new();
+
     public override void Initialize()
     {
         AvaloniaXamlLoader.Load(this);
@@ -16,10 +19,9 @@ public class App : Application
     public override void OnFrameworkInitializationCompleted()
     {
         if (ApplicationLifetime is IClassicDesktopStyleApplicationLifetime desktop)
-            desktop.MainWindow = new MainWindow
-            {
-                DataContext = new MainWindowViewModel()
-            };
+            desktop.MainWindow = new MainWindow();
+
+        Core = LaunchHandler.InitLaunch();
 
         base.OnFrameworkInitializationCompleted();
     }
---
+++ JackCraftLauncher/Class/Launch/LaunchHandler.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Net;
+using ProjBobcat.Class.Model.MicrosoftAuth;
+using ProjBobcat.DefaultComponent.Authenticator;
+using ProjBobcat.DefaultComponent.Launch;
+using ProjBobcat.DefaultComponent.Launch.GameCore;
+using ProjBobcat.DefaultComponent.Logging;
+
+namespace JackCraftLauncher.Class.Launch;
+
+public class LaunchHandler
+{
+    public static DefaultGameCore InitLaunch()
+    {
+        ServicePointManager.DefaultConnectionLimit = 1024;
+        var clientToken = new Guid("11451419-1981-0114-5141-919810114514");
+        var rootPath = "JCL/.minecraft/";
+        var core = new DefaultGameCore
+        {
+            ClientToken = clientToken,
+            RootPath = rootPath,
+            VersionLocator = new DefaultVersionLocator(rootPath, clientToken)
+            {
+                LauncherProfileParser = new DefaultLauncherProfileParser(rootPath, clientToken),
+                LauncherAccountParser = new DefaultLauncherAccountParser(rootPath, clientToken)
+            },
+            GameLogResolver = new DefaultGameLogResolver()
+        };
+        MicrosoftAuthenticator.Configure(new MicrosoftAuthenticatorAPISettings
+        {
+            ClientId = "f7bbc3a7-8c12-4f20-9ac4-0237cbff25e1",
+            TenentId = "consumers",
+            Scopes = new[] { "XboxLive.signin", "offline_access", "openid", "profile", "email" }
+        });
+        return core;
+    }
+}
