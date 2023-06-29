cd78039fc42c83b23a30b6b88787c29152bf2cc0
imJack6 <3486688394@qq.com>
2023-06-29 22:59:17 +0800

    🆙 添加启动游戏页面

---
+++ JackCraftLauncher/Views/MainMenus/StartGameMenu.axaml.cs
@@ -0,0 +1,32 @@
+﻿using Avalonia.Controls;
+
+namespace JackCraftLauncher.Views.MainMenus;
+
+public partial class StartGameMenu : UserControl
+{
+    public StartGameMenu()
+    {
+        InitializeComponent();
+        Localizer.Localizer.Instance.LoadLanguage("zh-CN");
+    }
+
+    public void ClearStartGameLog()
+    {
+        StartGameLogListBox.Items.Clear();
+    }
+
+    public void AddStartGameLog(string log)
+    {
+        StartGameLogListBox.Items.Add(log);
+        if (StartGameLogListBox.Scroll != null)
+        {
+            var listboxScroll = (ScrollViewer)StartGameLogListBox.Scroll;
+            listboxScroll.ScrollToEnd();
+        }
+    }
+
+    private void StartGameLogListBox_OnSelectionChanged(object? sender, SelectionChangedEventArgs e)
+    {
+        StartGameLogListBox.SelectedIndex = -1;
+    }
+}
