9a2aa42ae4f7728b202ff370f2f48c06e02ddeff
imJack6 <3486688394@qq.com>
2023-06-29 22:57:33 +0800

    💾 仅存储

---
+++ JackCraftLauncher/Views/MainMenus/SettingMenu.axaml.cs
@@ -0,0 +1,17 @@
+﻿using Avalonia.Controls;
+using Avalonia.Markup.Xaml;
+
+namespace JackCraftLauncher.Views.MainMenus;
+
+public partial class SettingMenu : UserControl
+{
+    public SettingMenu()
+    {
+        InitializeComponent();
+    }
+
+    private void InitializeComponent()
+    {
+        AvaloniaXamlLoader.Load(this);
+    }
+}
---
+++ JackCraftLauncher/Views/MainMenus/StartMenu.axaml.cs
@@ -0,0 +1,17 @@
+﻿using Avalonia.Controls;
+using Avalonia.Markup.Xaml;
+
+namespace JackCraftLauncher.Views.MainMenus;
+
+public partial class StartMenu : UserControl
+{
+    public StartMenu()
+    {
+        InitializeComponent();
+    }
+
+    private void InitializeComponent()
+    {
+        AvaloniaXamlLoader.Load(this);
+    }
+}
