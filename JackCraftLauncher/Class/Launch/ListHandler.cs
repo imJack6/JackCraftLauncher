f2f6412235bedd06cbf160856ace59117e9a831b
imJack6 <3486688394@qq.com>
2023-06-30 15:12:03 +0800

    🆙 新增List列表处理

---
+++ JackCraftLauncher/Class/Launch/ListHandler.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using JackCraftLauncher.Views.MainMenus;
+using ProjBobcat.Class.Model;
+
+namespace JackCraftLauncher.Class.Launch;
+
+public class ListHandler
+{
+    public static void RefreshLocalGameList()
+    {
+        try
+        {
+            List<VersionInfo> gameList = GetLocalGameList();
+            if (StartMenu.Instance != null)
+            {
+                if (gameList == null)
+                {
+                    GlobalVariable.LocalGameList = new();
+                    StartMenu.Instance.LocalGameListBox.ItemsSource = 
+                        GlobalVariable.LocalGameList.Select(x => x.Name ?? x.Id).ToList();
+                }
+                else
+                {
+                    if (GlobalVariable.LocalGameList != gameList)
+                    {
+                        GlobalVariable.LocalGameList = gameList;
+                        StartMenu.Instance.LocalGameListBox.ItemsSource = 
+                            GlobalVariable.LocalGameList.Select(x => x.Name ?? x.Id).ToList();
+                    }
+                }
+            }
+        }
+        catch (Exception)
+        {
+            // ignored
+        }
+    }
+    private static List<VersionInfo> GetLocalGameList()
+    {
+        List<VersionInfo> gameList = null!;
+        try
+        {
+            gameList = App.Core.VersionLocator.GetAllGames().ToList();
+        }
+        catch (Exception)
+        {
+            // ignored
+        }
+        return gameList;
+    }
+}
