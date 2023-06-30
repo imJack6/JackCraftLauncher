cd345722a0210b63aabb239bc61ced53066207ad
imJack6 <3486688394@qq.com>
2023-06-30 15:12:30 +0800

    🆙 新增全局本地游戏列表

--- JackCraftLauncher/Class/GlobalVariable.cs
+++ JackCraftLauncher/Class/GlobalVariable.cs
@@ -1,13 +1,15 @@
-﻿using JackCraftLauncher.Class.Launch;
+﻿using System.Collections.Generic;
+using JackCraftLauncher.Class.Launch;
+using ProjBobcat.Class.Model;
 
 namespace JackCraftLauncher.Class;
 
 public class GlobalVariable
 {
+    public static List<VersionInfo> LocalGameList { get; set; } = null!;
     public static DownloadSourceHandler.DownloadSourceEnum DownloadSourceEnum { get; set; } =
         DownloadSourceHandler.DownloadSourceEnum.BMCL; // 下载源
-
     public static int DownloadParallelismCount { get; set; } = 8; // 下载并行数
     public static int DownloadThreadCount { get; set; } = 8; // 下载线程数
-    public static int DownloadRetryCount { get; set; } = 3; // 下载重试次数
+    public static int DownloadRetryCount { get; set; } = 4; // 下载重试次数
 }
