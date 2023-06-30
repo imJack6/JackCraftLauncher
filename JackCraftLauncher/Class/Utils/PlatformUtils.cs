020c835346cd2b19a48b68eae4982ed6ca20f8a9
imJack6 <3486688394@qq.com>
2023-06-30 15:08:29 +0800

    🆙 跨平台设计

---
+++ JackCraftLauncher/Class/Utils/PlatformUtils.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace JackCraftLauncher.Class.Utils;
+
+public class PlatformUtils
+{
+    public enum OperatingSystem
+    {
+        Windows,
+        Linux,
+        Macos
+    }
+    
+    public static OperatingSystem GetOperatingSystem()
+    {
+#if WINDOWS
+        return OperatingSystem.Windows;
+#elif LINUX
+        return OperatingSystem.Linux;
+#elif OSX
+        return OperatingSystem.Macos;
+#endif
+    }
+}
