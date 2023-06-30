f0cb3c0b2394bc119353eb95de55047d63012db2
imJack6 <3486688394@qq.com>
2023-06-30 15:13:20 +0800

    🆙 新增Dialog

---
+++ JackCraftLauncher/Class/Models/WarningTemplateModel.cs
@@ -0,0 +1,13 @@
+﻿namespace JackCraftLauncher.Class.Models;
+
+public class WarningTemplateModel
+{
+    public WarningTemplateModel(string header1,string content1)
+    {
+        Header1 = header1;
+        Content1 = content1;
+    }
+
+    public string Header1 { get; set; }
+    public string Content1 { get; set; }
+}
