13c7556d76fbcb22e6a778728ca28298d87b094b
imJack6 <3486688394@qq.com>
2023-07-01 19:33:05 +0800

    🆙 全局常量

---
+++ JackCraftLauncher/Class/GlobalConstants.cs
@@ -0,0 +1,17 @@
+﻿namespace JackCraftLauncher.Class;
+
+public static class GlobalConstants
+{
+    public const string ConfigThemeNode = "LauncherSettings.ThemeMode";
+
+    public const string ConfigDownloadSourceNode = "DownloadSettings.DownloadSource";
+    public const string ConfigDownloadParallelismCountNode = "DownloadSettings.ParallelismCount";
+    public const string ConfigDownloadThreadCountNode = "DownloadSettings.ThreadCount";
+    public const string ConfigDownloadRetryCountNode = "DownloadSettings.RetryCount";
+
+    public const string ConfigSelectedJavaIndexNode = "GlobalGameSettings.SelectedJavaIndex";
+    public const string ConfigJavaPathListNode = "GlobalGameSettings.JavaPathList";
+    public const string ConfigGameGcTypeNode = "GlobalGameSettings.GcType";
+    public const string ConfigGameResolutionWidthNode = "GlobalGameSettings.ResolutionWidth";
+    public const string ConfigGameResolutionHeightNode = "GlobalGameSettings.ResolutionHeight";
+}
