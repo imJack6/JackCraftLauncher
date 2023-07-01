bea0303e4356cf4021c3aaa08ec2f1a1c6420af6
imJack6 <3486688394@qq.com>
2023-07-01 19:32:07 +0800

    🆙 仅存储

---
+++ JackCraftLauncher/Views/MainMenus/LoginMenus/ThirdPartyLoginControl.axaml.cs
@@ -0,0 +1,11 @@
+﻿using Avalonia.Controls;
+
+namespace JackCraftLauncher.Views.MainMenus.LoginMenus;
+
+public partial class ThirdPartyLoginControl : UserControl
+{
+    public ThirdPartyLoginControl()
+    {
+        InitializeComponent();
+    }
+}
