8f68adbad6cf8776ef5796e11c93176b8b619c36
imJack6 <3486688394@qq.com>
2023-07-04 23:45:34 +0800

    🆙 添加加解密

---
+++ JackCraftLauncher/Class/Launch/EncryptHandler.cs
@@ -0,0 +1,35 @@
+﻿using System.Reflection;
+using System.Text;
+
+namespace JackCraftLauncher.Class.Launch;
+
+public class EncryptHandler
+{
+    public static string JcEncrypt(string rawStr)
+    {
+        var encryptStr = string.Empty;
+        var encryptKey = Assembly.GetEntryAssembly()!.GetName().Name!;
+        var encryptKeyLength = encryptKey.Length;
+        var strLength = rawStr.Length;
+        for (var i = 0; i < strLength; i++)
+        {
+            var k = i % encryptKeyLength;
+            encryptStr += (char)(rawStr[i] ^ encryptKey[k]);
+        }
+        return Convert.ToBase64String(Encoding.UTF8.GetBytes(encryptStr));
+    }
+    public static string JcDecrypt(string encryptStr)
+    {
+        string str = Encoding.UTF8.GetString(Convert.FromBase64String(encryptStr));
+        var decryptStr = string.Empty;
+        var encryptKey = Assembly.GetEntryAssembly()!.GetName().Name!;
+        var encryptKeyLength = encryptKey.Length;
+        var strLength = str.Length;
+        for (var i = 0; i < strLength; i++)
+        {
+            var k = i % encryptKeyLength;
+            decryptStr += (char)(str[i] ^ encryptKey[k]);
+        }
+        return decryptStr;
+    }
+}
