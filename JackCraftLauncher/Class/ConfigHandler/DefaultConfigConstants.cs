dc16d64b95e914ea4829c2ca63a33c2f3563aa73
imJack6 <3486688394@qq.com>
2023-07-05 22:24:19 +0800

    📝 移动配置文件节点常量

---
+++ JackCraftLauncher/Class/ConfigHandler/DefaultConfigConstants.cs
@@ -0,0 +1,26 @@
+﻿namespace JackCraftLauncher.Class.ConfigHandler;
+
+public static class DefaultConfigConstants
+{
+    public class GlobalGameSettingsNodes
+    {
+        public const string SelectedJavaIndexNode = "GlobalGameSettings.SelectedJavaIndex";
+        public const string JavaPathListNode = "GlobalGameSettings.JavaPathList";
+        public const string GcTypeNode = "GlobalGameSettings.GcType";
+        public const string ResolutionWidthNode = "GlobalGameSettings.ResolutionWidth";
+        public const string ResolutionHeightNode = "GlobalGameSettings.ResolutionHeight";
+    }
+
+    public class DownloadSettingsNodes
+    {
+        public const string DownloadSourceNode = "DownloadSettings.DownloadSource";
+        public const string MaxDegreeOfParallelismCountNode = "DownloadSettings.MaxDegreeOfParallelismCount";
+        public const string PartsCountNode = "DownloadSettings.PartsCount";
+        public const string RetryCountNode = "DownloadSettings.RetryCount";
+    }
+
+    public class LauncherSettingsNodes
+    {
+        public const string ThemeNode = "LauncherSettings.ThemeMode";
+    }
+}
--- JackCraftLauncher/Class/GlobalConstants.cs
+++ JackCraftLauncher/Class/GlobalConstants.cs
@@ -2,16 +2,4 @@
 
 public static class GlobalConstants
 {
-    public const string ConfigThemeNode = "LauncherSettings.ThemeMode";
-
-    public const string ConfigDownloadSourceNode = "DownloadSettings.DownloadSource";
-    public const string ConfigDownloadParallelismCountNode = "DownloadSettings.ParallelismCount";
-    public const string ConfigDownloadThreadCountNode = "DownloadSettings.ThreadCount";
-    public const string ConfigDownloadRetryCountNode = "DownloadSettings.RetryCount";
-
-    public const string ConfigSelectedJavaIndexNode = "GlobalGameSettings.SelectedJavaIndex";
-    public const string ConfigJavaPathListNode = "GlobalGameSettings.JavaPathList";
-    public const string ConfigGameGcTypeNode = "GlobalGameSettings.GcType";
-    public const string ConfigGameResolutionWidthNode = "GlobalGameSettings.ResolutionWidth";
-    public const string ConfigGameResolutionHeightNode = "GlobalGameSettings.ResolutionHeight";
 }
