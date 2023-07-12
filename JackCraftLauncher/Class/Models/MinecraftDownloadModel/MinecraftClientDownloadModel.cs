21b77d53c2cc8cfa144d76055fcd386cb880c75f
imJack6 <3486688394@qq.com>
2023-07-12 19:43:47 +0800

    🆙 添加客户端下载模型

---
+++ JackCraftLauncher/Class/Models/MinecraftDownloadModel/MinecraftClientDownloadModel.cs
@@ -0,0 +1,20 @@
+﻿using System.Text.Json.Serialization;
+
+namespace JackCraftLauncher.Class.Models.MinecraftDownloadModel;
+
+public class MinecraftClientDownloadModel
+{
+    [JsonPropertyName("downloads")] public Downloads Downloads { get; set; }
+}
+
+public class Downloads
+{
+    [JsonPropertyName("client")] public Client Client { get; set; }
+}
+
+public class Client
+{
+    [JsonPropertyName("sha1")] public string Sha1 { get; set; }
+    [JsonPropertyName("size")] public int Size { get; set; }
+    [JsonPropertyName("url")] public string Url { get; set; }
+}
